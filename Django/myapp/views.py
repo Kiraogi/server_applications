28f7206f0943f7009f947605dc46f3212db516ca
Kiraogi <golokron3@yandex.ru>
2026-03-09 19:23:37 +0300

    ver 0.05

--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Привет Django!')
+    return render (request, 'myapp/index.html', {'title': 'Главная страница'})
 
 def about(request):
-    return render (request, 'about.html', {'title': 'О нас'})
+    return render (request, 'myapp/about.html', {'title': 'О нас'})
 
+def products(request):
+    items = ["Ноутбук", "Смартфон", "Клавиатура"]
+    return render(request, 'myapp/products.html', {'items': items})
